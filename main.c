681ac7bc8c5541557b95eb29e86f2830a9404fb1
Atamba-Chinaza <110666732+Atamba-Chinaza@users.noreply.github.com>
2022-10-19 03:02:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,130 +1,55 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * main - tests the _printf function
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: Always 0.
  */
-int _isdigit(int c)
+int main(void)
 {
-	return (c >= '0' && c <= '9');
-}
+	int code;
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
+	printf("Numeric:\n");
+	printf("============\n");
+/* %i without param */
+	code = printf("Std: %i.\n");
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %i.\n");
+	printf("Our Returns: %d\n", code);
 
-	while (*s++)
-		i++;
-	return (i);
-}
+	code = printf("Std: %i.\n", 0);
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %i.\n", 0);
+	printf("Our Returns: %d\n", code);
 
-/**
- * print_number - prints a number with options
- * @str: the base number as a string
- * @params: the parameter struct
- *
- * Return: chars printed
- */
-int print_number(char *str, params_t *params)
-{
-	unsigned int i = _strlen(str);
-	int neg = (!params->unsign && *str == '-');
+	code = printf("Std: %i.\n", 67);
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %i.\n", 67);
+	printf("Our Returns: %d\n", code);
 
-	if (!params->precision && *str == '0' && !str[1])
-		str = "";
-	if (neg)
-	{
-		str++;
-		i--;
-	}
-	if (params->precision != UINT_MAX)
-		while (i++ < params->precision)
-			*--str = '0';
-	if (neg)
-		*--str = '-';
+	code = printf("Std: %i.\n", 127);
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %i.\n", 127);
+	printf("Our Returns: %d\n", code);
 
-	if (!params->minus_flag)
-		return (print_number_right_shift(str, params));
-	else
-		return (print_number_left_shift(str, params));
-}
+	code = printf("Std: %d.\n", 0 % 10);
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %d.\n", 0 % 10);
+	printf("Our Returns: %d\n", code);
 
-/**
- * print_number_right_shift - prints a number with options
- * @str: the base number as a string
- * @params: the parameter struct
- *
- * Return: chars printed
- */
-int print_number_right_shift(char *str, params_t *params)
-{
-	unsigned int n = 0, neg, neg2, i = _strlen(str);
-	char pad_char = ' ';
+	printf("\nCharacters:\n");
+	printf("============\n");
 
-	if (params->zero_flag && !params->minus_flag)
-		pad_char = '0';
-	neg = neg2 = (!params->unsign && *str == '-');
-	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
-		str++;
-	else
-		neg = 0;
-	if ((params->plus_flag && !neg2) ||
-		(!params->plus_flag && params->space_flag && !neg2))
-		i++;
-	if (neg && pad_char == '0')
-		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
-		n += _putchar('+');
-	else if (!params->plus_flag && params->space_flag && !neg2 &&
-		!params->unsign && params->zero_flag)
-		n += _putchar(' ');
-	while (i++ < params->width)
-		n += _putchar(pad_char);
-	if (neg && pad_char == ' ')
-		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
-		n += _putchar('+');
-	else if (!params->plus_flag && params->space_flag && !neg2 &&
-		!params->unsign && !params->zero_flag)
-		n += _putchar(' ');
-	n += _puts(str);
-	return (n);
-}
+	code = printf("Std: %%.\n");
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: %%.\n");
+	printf("Our Returns: %d\n", code);
 
-/**
- * print_number_left_shift - prints a number with options
- * @str: the base number as a string
- * @params: the parameter struct
- *
- * Return: chars printed
- */
-int print_number_left_shift(char *str, params_t *params)
-{
-	unsigned int n = 0, neg, neg2, i = _strlen(str);
-	char pad_char = ' ';
+	code = printf("Std: Cu%cio.us\n", '7');
+	printf("Std Returns: %d\n", code);
+	code = _printf("Our: Cu%cio.us\n", '7');
+	printf("Our Returns: %d\n", code);
 
-	if (params->zero_flag && !params->minus_flag)
-		pad_char = '0';
-	neg = neg2 = (!params->unsign && *str == '-');
-	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
-		str++;
-	else
-		neg = 0;
-
-	if (params->plus_flag && !neg2 && !params->unsign)
-		n += _putchar('+'), i++;
-	else if (params->space_flag && !neg2 && !params->unsign)
-		n += _putchar(' '), i++;
-	n += _puts(str);
-	while (i++ < params->width)
-		n += _putchar(pad_char);
-	return (n);
+	return (0);
 }
